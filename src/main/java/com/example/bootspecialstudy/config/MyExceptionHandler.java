47d2a125c3f7931cd93dc82fc607ede24647ce35
tjdans34568 <tjdans34568@gmail.com>
2021-12-27 16:03:43 +0900

    error Handler 처리

--- src/main/java/com/example/bootspecialstudy/config/MyExceptionHandler.java
+++ src/main/java/com/example/bootspecialstudy/config/MyExceptionHandler.java
@@ -1,15 +1,16 @@
 package com.example.bootspecialstudy.config;
 
-// Exception을 낚아채기
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+
+// Exception 낚아채기
 @RestController
 //@RestControllerAdvice 내부에 @ControllerAdvice 를 상속받고 있음
-@ControllerAdvice // @ControllerAdvice는 모든 @Controller 즉, 전역에서 발생할 수 있는 예외를 잡아 처리해주는 annotation이다.
+@ControllerAdvice // @ControllerAdvice 는 모든 @Controller 즉, 전역에서 발생할 수 있는 예외를 잡아 처리해주는 annotation 이다.
 public class MyExceptionHandler {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
